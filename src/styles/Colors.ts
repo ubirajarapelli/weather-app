cac55c8c4c866b5d22545b4b2d240b1f336ad9ec
Ubirajara Pelli <ubirajaracpelli@gmail.com>
2024-08-22 16:37:04 -0300

    feat: add inicial styles

---
+++ src/styles/Colors.ts
@@ -0,0 +1,131 @@
+export const colors = {
+  app: {
+    clear: {
+      morning: {
+        background: "#87CEEB",
+        card: "#38aede",
+        text: "#ffffff",
+        darkText: "#15607f",
+      },
+      night: {
+        background: "#2C3E50",
+        card: "#212e3c",
+        text: "#ECF0F1",
+      },
+    },
+    cloudy: {
+      morning: {
+        background: "#D3D3D3",
+        card: "#9e9e9e",
+        text: "#2C3E50",
+      },
+      night: {
+        background: "#34495E",
+        card: "#273746",
+        text: "#ECF0F1",
+      },
+    },
+    rainy: {
+      morning: {
+        background: "#5D6D7E",
+        card: "#46525e",
+        text: "#ffffff",
+      },
+      night: {
+        background: "#2C3E50",
+        card: "#212e3c",
+        text: "#ECF0F1",
+      },
+    },
+    stormy: {
+      morning: {
+        background: "#4B4B4B",
+        card: "#383838",
+        text: "#ffffff",
+      },
+      night: {
+        background: "#1C2833",
+        card: "#151e26",
+        text: "#ECF0F1",
+      },
+    },
+    snowy: {
+      morning: {
+        background: "#E0E0E0",
+        card: "#a8a8a8",
+        text: "#2C3E50",
+      },
+      night: {
+        background: "#BDC3C7",
+        card: "#89939a",
+        text: "#2C3E50",
+      },
+    },
+  },
+  web: {
+    clear: {
+      morning: {
+        background: "#FFEE93",
+        card: "#ffde2e",
+        text: "#4A4A4A",
+      },
+      night: {
+        background: "#FFB347",
+        card: "#f49000",
+        text: "#2E2E2E",
+      },
+    },
+    cloudy: {
+      morning: {
+        background: "#BEC3C7",
+        card: "#8a939a",
+        text: "#2E2E2E",
+      },
+      night: {
+        background: "#8E9297",
+        card: "#696d72",
+        text: "#F2F2F2",
+      },
+    },
+    rainy: {
+      morning: {
+        background: "#92A8D1",
+        card: "#5476b7",
+        text: "#2E2E2E",
+      },
+      night: {
+        background: "#34495E",
+        card: "#273746",
+        text: "#ECF0F1",
+      },
+    },
+    stormy: {
+      morning: {
+        background: "#5D6D7E",
+        card: "#46525e",
+        text: "#F2F2F2",
+      },
+      night: {
+        background: "#2E4053",
+        card: "#22303e",
+        text: "#D5DBDB",
+      },
+    },
+    snowy: {
+      morning: {
+        background: "#E0E0E0",
+        card: "#a8a8a8",
+        text: "#2E2E2E",
+      },
+      night: {
+        background: "#BDC3C7",
+        card: "#89939a",
+        text: "#2E2E2E",
+      },
+    },
+  },
+  text: {
+    dark: "#1f1e31",
+    light: "#999999",
+  },
+};
